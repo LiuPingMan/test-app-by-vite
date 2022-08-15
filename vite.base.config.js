fbfbe0a1750dd9604c8f75feea9a7e830eb618e2
LiuPingMan <g1792202861@gmail.com>
2022-08-15 21:00:51 +0800

    feat:配置静态资源打包

--- vite.base.config.js
+++ vite.base.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 //css语法降级、补全前缀
 import postcssPresetEnv from "postcss-preset-env"
+import path from "path"
 
 export default defineConfig({
   plugins: [vue()],
@@ -29,5 +30,24 @@ export default defineConfig({
     postcss: {
       plugins: [postcssPresetEnv()],
     },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    //配置rollup的构建策略
+    rollupOptions: {
+      //控制输出
+      output: {
+        //配置静态资源的打包后文件名
+        assetFileNames: "[name].[hash].[ext]",
+      },
+    },
+    assetsInlineLimit: 20 * 1024, //将小于20kb的静态资源打包为base64
+    outDir: "dist", //配置打包输出的文件夹名,默认为dist
+    assetsDir: "assets", //配置静态资源打包目录，默认为assets
+    emptyOutDir: true, //打包前清空打包目录，默认为true
   },
 })
